577c7d8a09b479643ee55ac14f598295a094d02a
Chris Graham <chisnet@hotmail.com>
2014-10-28 23:23:04 +0000

    Empty module

---
+++ pretty.js
@@ -0,0 +1,11 @@
+(function(window){
+    var pretty = {
+        
+    };
+    if(typeof define === 'function' && define.amd) {
+        define(pretty);
+    }
+    else {
+        window.pretty = pretty;
+    }
+})(window);
